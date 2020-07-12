9a087130c93337508e5fd7fc59434a3119448ffa
Nic5472K <32753860+Nic5472K@users.noreply.github.com>
2020-07-12 21:41:47 +1000

    Add files via upload

---
+++ Z000_IJCNN/NicK008a_IRCTLSTM_LM.py
@@ -0,0 +1,267 @@
+###===###
+# NicK of the ANU
+#---
+# Comments:
+# (1)   A vanilla IRC-TLSTM language model for PTB
+#
+# (2)   Mainly built on top of SalesForce's code (SF)
+#       https://github.com/salesforce/awd-lstm-lm
+#
+# (3)   The PTB dataset can be found in many places
+#       such as
+#       https://github.com/hjc18/language_modeling_lstm/tree/master/input
+#
+# (4)   Original results gained from
+#           PyTorch version 0.4.1.post2
+#       Not PyTorch version 1.1.0
+
+###===###
+import  torch
+import  torch.nn        as      nn
+#
+#---
+import  argparse
+import  time
+import  math
+import  numpy           as      np
+#
+#---
+import  SF_data
+import  NicK008b_model_IRCTLSTM  as      model
+
+from    SF_utils        import  batchify, get_batch, repackage_hidden
+
+###===###
+parser = argparse.ArgumentParser(description='PyTorch PennTreeBank RNN/LSTM Language Model')
+#
+#---
+# The hyperparameters that we care about in our paper
+parser.add_argument('--emsize',     type=int,   default=650,
+                                    help='size of word embeddings')
+parser.add_argument('--nhid',       type=int,   default=650,
+                                    help='number of hidden units per layer')
+parser.add_argument('--nlayers',    type=int,   default=2,
+                                    help='number of layers')
+parser.add_argument('--epochs',     type=int,   default=50,
+                                    help='upper epoch limit')
+#
+#---
+# The rest, see the SF code
+parser.add_argument('--data',               type=str,   default='./penn/')
+parser.add_argument('--model',              type=str,   default='LSTM')    # <--- dummy placeholder
+parser.add_argument('--lr',                 type=float, default=30)
+parser.add_argument('--clip',               type=float, default=0.25)
+parser.add_argument('--batch_size',         type=int,   default=20, metavar='N')
+parser.add_argument('--bptt',               type=int,   default=70)
+parser.add_argument('--dropout',            type=float, default=0.4)
+parser.add_argument('--dropouth',           type=float, default=0.25)
+parser.add_argument('--dropouti',           type=float, default=0.4)
+parser.add_argument('--dropoute',           type=float, default=0.1)
+parser.add_argument('--wdrop',              type=float, default=0.25)
+parser.add_argument('--seed',               type=int,   default=141)
+parser.add_argument('--nonmono',            type=int,   default=5)
+parser.add_argument('--cuda',               action='store_false')
+parser.add_argument('--log-interval',       type=int,   default=40, metavar='N')
+randomhash = ''.join(str(time.time()).split('.'))
+parser.add_argument('--save',               type=str,   default=randomhash+'.pt')
+parser.add_argument('--alpha',              type=float, default=2)
+parser.add_argument('--beta',               type=float, default=1)
+parser.add_argument('--wdecay',             type=float, default=1.2e-6)
+parser.add_argument('--resume',             type=str,   default='')
+parser.add_argument('--optimizer',          type=str,   default='sgd')
+parser.add_argument('--when', nargs="+",    type=int,   default=[-1])
+args = parser.parse_args()
+args.tied = True
+#
+#---
+np.random.seed(     args.seed)
+torch.manual_seed(  args.seed)
+
+###===###
+# Building the Dataset
+#---
+# See the SF code
+def model_save(fn):
+    with open(fn, 'wb') as f:
+        torch.save([model, criterion, optimizer], f)
+def model_load(fn):
+    global model, criterion, optimizer
+    with open(fn, 'rb') as f:
+        model, criterion, optimizer = torch.load(f)
+import os
+import hashlib
+fn = 'corpus.{}.data'.format(hashlib.md5(args.data.encode()).hexdigest())
+if os.path.exists(fn):
+    print('Loading cached dataset...')
+    corpus = torch.load(fn)
+else:
+    print('Producing dataset...')
+    corpus = SF_data.Corpus(args.data)
+    torch.save(corpus, fn)
+eval_batch_size = 10
+test_batch_size = 1
+train_data = batchify(corpus.train, args.batch_size, args)
+val_data = batchify(corpus.valid, eval_batch_size, args)
+test_data = batchify(corpus.test, test_batch_size, args)
+
+###===###
+# Building the model
+#---
+# See the SF code
+from splitcross import SplitCrossEntropyLoss
+criterion = None
+ntokens = len(corpus.dictionary)
+model = model.RNNModel(args.model, ntokens, args.emsize, args.nhid, args.nlayers, args.dropout, args.dropouth, args.dropouti, args.dropoute, args.wdrop, args.tied)
+###
+if args.resume:
+    print('Resuming model ...')
+    model_load(args.resume)
+    optimizer.param_groups[0]['lr'] = args.lr
+    model.dropouti, model.dropouth, model.dropout, args.dropoute = args.dropouti, args.dropouth, args.dropout, args.dropoute
+    if args.wdrop:
+        from weight_drop import WeightDrop
+        for rnn in model.rnns:
+            if type(rnn) == WeightDrop: rnn.dropout = args.wdrop
+            elif rnn.zoneout > 0: rnn.zoneout = args.wdrop
+if not criterion:
+    splits = []
+    if ntokens > 500000:
+        splits = [4200, 35000, 180000]
+    elif ntokens > 75000:
+        splits = [2800, 20000, 76000]
+    print('Using', splits)
+    criterion = SplitCrossEntropyLoss(args.emsize, splits=splits, verbose=False)
+if args.cuda:
+    model = model.cuda()
+    criterion = criterion.cuda()
+params = list(model.parameters()) + list(criterion.parameters())
+
+###===###
+# For training and testing
+#---
+# See the SF code
+
+def evaluate(data_source, batch_size=10):
+    model.eval()
+    if args.model == 'QRNN': model.reset()
+    total_loss = 0
+    ntokens = len(corpus.dictionary)
+    hidden = model.init_hidden(batch_size)
+    for i in range(0, data_source.size(0) - 1, args.bptt):
+        data, targets = get_batch(data_source, i, args, evaluation=True)
+        output, hidden = model(data, hidden)
+        total_loss += len(data) * criterion(model.decoder.weight, model.decoder.bias, output, targets).data
+        hidden = repackage_hidden(hidden)
+    return total_loss.item() / len(data_source)
+
+
+def train():
+    if args.model == 'QRNN': model.reset()
+    total_loss = 0
+    start_time = time.time()
+    ntokens = len(corpus.dictionary)
+    hidden = model.init_hidden(args.batch_size)
+    batch, i = 0, 0
+    while i < train_data.size(0) - 1 - 1:
+        bptt = args.bptt if np.random.random() < 0.95 else args.bptt / 2.
+        seq_len = max(5, int(np.random.normal(bptt, 5)))
+        lr2 = optimizer.param_groups[0]['lr']
+        optimizer.param_groups[0]['lr'] = lr2 * seq_len / args.bptt
+        model.train()
+        data, targets = get_batch(train_data, i, args, seq_len=seq_len)
+        hidden = repackage_hidden(hidden)
+        optimizer.zero_grad()
+        output, hidden, rnn_hs, dropped_rnn_hs = model(data, hidden, return_h=True)
+        raw_loss = criterion(model.decoder.weight, model.decoder.bias, output, targets)
+        loss = raw_loss
+        if args.alpha: loss = loss + sum(args.alpha * dropped_rnn_h.pow(2).mean() for dropped_rnn_h in dropped_rnn_hs[-1:])
+        if args.beta: loss = loss + sum(args.beta * (rnn_h[1:] - rnn_h[:-1]).pow(2).mean() for rnn_h in rnn_hs[-1:])
+        loss.backward()
+        if args.clip: torch.nn.utils.clip_grad_norm_(params, args.clip)
+        optimizer.step()
+        total_loss += raw_loss.data
+        optimizer.param_groups[0]['lr'] = lr2
+        if batch % args.log_interval == 0 and batch > 0:
+            cur_loss = total_loss.item() / args.log_interval
+            elapsed = time.time() - start_time
+            print('| epoch {:3d} | {:5d}/{:5d} batches | lr {:05.5f} | ms/batch {:5.2f} | '
+                    'loss {:5.2f} | ppl {:8.2f} | bpc {:8.3f}'.format(
+                epoch, batch, len(train_data) // args.bptt, optimizer.param_groups[0]['lr'],
+                elapsed * 1000 / args.log_interval, cur_loss, math.exp(cur_loss), cur_loss / math.log(2)))
+            total_loss = 0
+            start_time = time.time()
+        batch += 1
+        i += seq_len
+
+###===###
+# The actual stuff starts here
+#---
+# See the SF code
+lr = args.lr
+best_val_loss = []
+stored_loss = 100000000
+try:
+    optimizer = None
+    if args.optimizer == 'sgd':
+        optimizer = torch.optim.SGD(params, lr=args.lr, weight_decay=args.wdecay)
+    if args.optimizer == 'adam':
+        optimizer = torch.optim.Adam(params, lr=args.lr, weight_decay=args.wdecay)
+    for epoch in range(1, args.epochs+1):
+        epoch_start_time = time.time()
+        train()
+        if 't0' in optimizer.param_groups[0]:
+            tmp = {}
+            for prm in model.parameters():
+                tmp[prm] = prm.data.clone()
+                if 'ax' in optimizer.state[prm]:
+                    prm.data = optimizer.state[prm]['ax'].clone()
+
+            val_loss2 = evaluate(val_data)
+            print('-' * 89)
+            print('| end of epoch {:3d} | time: {:5.2f}s | valid loss {:5.2f} | '
+                'valid ppl {:8.2f} | valid bpc {:8.3f}'.format(
+                    epoch, (time.time() - epoch_start_time), val_loss2, math.exp(val_loss2), val_loss2 / math.log(2)))
+            print('-' * 89)
+            if val_loss2 < stored_loss:
+                model_save(args.save)
+                print('Saving Averaged!')
+                stored_loss = val_loss2
+
+            for prm in model.parameters():
+                prm.data = tmp[prm].clone()
+        else:
+            val_loss = evaluate(val_data, eval_batch_size)
+            print('-' * 89)
+            print('| end of epoch {:3d} | time: {:5.2f}s | valid loss {:5.2f} | '
+                'valid ppl {:8.2f} | valid bpc {:8.3f}'.format(
+              epoch, (time.time() - epoch_start_time), val_loss, math.exp(val_loss), val_loss / math.log(2)))
+            print('-' * 89)
+            if val_loss < stored_loss:
+                model_save(args.save)
+                print('Saving model (new best validation)')
+                stored_loss = val_loss
+            if epoch == 30:
+                print('Switching to ASGD')
+                optimizer = torch.optim.ASGD(model.parameters(), lr=args.lr, t0=0, lambd=0., weight_decay=args.wdecay)
+            if epoch in args.when:
+                print('Saving model before learning rate decreased')
+                model_save('{}.e{}'.format(args.save, epoch))
+                print('Dividing learning rate by 10')
+                optimizer.param_groups[0]['lr'] /= 10.
+            best_val_loss.append(val_loss)
+        if np.mod(epoch, 5) == 0:
+            torch.save(model.state_dict(), 'L3E50_EINS_prm_epoch{}'.format(epoch))
+            
+###===###
+# Post training related
+#---
+# See the SF code
+except KeyboardInterrupt:
+    print('-' * 89)
+    print('Exiting from training early')
+model_load(args.save)
+test_loss = evaluate(test_data, test_batch_size)
+print('=' * 89)
+print('| End of training | test loss {:5.2f} | test ppl {:8.2f} | test bpc {:8.3f}'.format(
+    test_loss, math.exp(test_loss), test_loss / math.log(2)))
+print('=' * 89)
---
+++ Z000_IJCNN/NicK008b_model_IRCTLSTM.py
@@ -0,0 +1,187 @@
+###===###
+# NicK of the ANU
+#---
+# Comments:
+# (1)   The backbone RNN code for IRC-TLSTM
+#
+# (2)   Mainly built on top of SalesForce's code (SF)
+#       https://github.com/salesforce/awd-lstm-lm
+
+###===###
+import  torch
+import  torch.nn                as      nn
+import  torch.nn.functional     as      F
+#
+#---
+import  math
+#
+#---
+from    SF_embed_regularize     import  embedded_dropout
+from    SF_locked_dropout       import  LockedDropout
+from    SF_weight_drop2         import  WeightDrop
+
+###===###
+class Method(nn.Module):
+    def __init__(self,ID, HD, dropout):
+        super(Method, self).__init__()
+
+        self.ih = nn.Linear(ID, HD * 2, bias = False)
+
+        self.WA = nn.Linear(ID, HD, bias = False)
+
+        self.uV_raw = nn.Parameter(torch.randn(HD))
+        
+        self.dropout  = dropout
+
+        self.HD = HD
+        self.ID = ID
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        stdv = 1.0 / math.sqrt(self.HD)
+        for weight in self.parameters():
+            nn.init.uniform_(weight, -stdv, stdv)
+
+        self.PFB = nn.Parameter(torch.ones(self.HD))
+        self.uV2 = nn.Parameter(torch.ones(self.ID) * 2)
+
+    def forward(self, x, HC):
+
+        raw_w2 = self.uV_raw
+        w2 = None
+        w2 = F.dropout(raw_w2, p=self.dropout, training=self.training)
+        setattr(self, 'uV', w2)
+
+        (Q_k, S_k) = HC
+        Q_k = Q_k.squeeze(0)
+        S_k = S_k.squeeze(0)
+        tspan = x.shape[0]
+
+        YALL = []
+
+        for itr in range(tspan):
+            
+            X_k = x[itr]
+
+            V_k = X_k + torch.sigmoid(self.uV2) * torch.tanh(self.uV) * Q_k
+
+            Fi, Oi = self.ih(V_k).chunk(2, dim = 1)
+            Ai = self.WA(X_k)
+
+            F_k = torch.sigmoid(Fi + self.PFB)
+            A_k = Ai
+            S_k = F_k * S_k + (1 - F_k) * A_k
+            
+            O_k = torch.sigmoid(Oi)
+            Q_k = X_k
+            N_k = O_k * S_k
+
+            YALL.append(N_k.unsqueeze(0))
+
+        Q_k = Q_k.unsqueeze(0)
+        S_k = S_k.unsqueeze(0)
+        
+        YALL2 = torch.cat(YALL)
+
+        return YALL2, (Q_k, S_k)
+
+class RNNModel(nn.Module):
+    """Container module with an encoder, a recurrent module, and a decoder."""
+
+    def __init__(self, rnn_type, ntoken, ninp, nhid, nlayers, dropout=0.5, dropouth=0.5, dropouti=0.5, dropoute=0.1, wdrop=0, tie_weights=False):
+        super(RNNModel, self).__init__()
+        self.lockdrop = LockedDropout()
+        self.idrop = nn.Dropout(dropouti)
+        self.hdrop = nn.Dropout(dropouth)
+        self.drop = nn.Dropout(dropout)
+        self.encoder = nn.Embedding(ntoken, ninp)
+        assert rnn_type in ['LSTM', 'QRNN', 'GRU'], 'RNN type is not supported'
+        if rnn_type == 'LSTM':
+            self.rnns = [Method(ninp if l == 0 else nhid, nhid if l != nlayers - 1 else (ninp if tie_weights else nhid), dropout = wdrop) for l in range(nlayers)]
+            #if wdrop:
+            #    self.rnns = [WeightDrop(rnn, ['weight_hh_l0'], dropout=wdrop) for rnn in self.rnns]
+        if rnn_type == 'GRU':
+            self.rnns = [torch.nn.GRU(ninp if l == 0 else nhid, nhid if l != nlayers - 1 else ninp, 1, dropout=0) for l in range(nlayers)]
+            if wdrop:
+                self.rnns = [WeightDrop(rnn, ['weight_hh_l0'], dropout=wdrop) for rnn in self.rnns]
+        elif rnn_type == 'QRNN':
+            from torchqrnn import QRNNLayer
+            self.rnns = [QRNNLayer(input_size=ninp if l == 0 else nhid, hidden_size=nhid if l != nlayers - 1 else (ninp if tie_weights else nhid), save_prev_x=True, zoneout=0, window=2 if l == 0 else 1, output_gate=True) for l in range(nlayers)]
+            for rnn in self.rnns:
+                rnn.linear = WeightDrop(rnn.linear, ['weight'], dropout=wdrop)
+        print(self.rnns)
+        self.rnns = torch.nn.ModuleList(self.rnns)
+        self.decoder = nn.Linear(nhid, ntoken)
+
+        # Optionally tie weights as in:
+        # "Using the Output Embedding to Improve Language Models" (Press & Wolf 2016)
+        # https://arxiv.org/abs/1608.05859
+        # and
+        # "Tying Word Vectors and Word Classifiers: A Loss Framework for Language Modeling" (Inan et al. 2016)
+        # https://arxiv.org/abs/1611.01462
+        if tie_weights:
+            #if nhid != ninp:
+            #    raise ValueError('When using the tied flag, nhid must be equal to emsize')
+            self.decoder.weight = self.encoder.weight
+
+        self.init_weights()
+
+        self.rnn_type = rnn_type
+        self.ninp = ninp
+        self.nhid = nhid
+        self.nlayers = nlayers
+        self.dropout = dropout
+        self.dropouti = dropouti
+        self.dropouth = dropouth
+        self.dropoute = dropoute
+        self.tie_weights = tie_weights
+
+    def reset(self):
+        if self.rnn_type == 'QRNN': [r.reset() for r in self.rnns]
+
+    def init_weights(self):
+        initrange = 0.1
+        self.encoder.weight.data.uniform_(-initrange, initrange)
+        self.decoder.bias.data.fill_(0)
+        self.decoder.weight.data.uniform_(-initrange, initrange)
+
+    def forward(self, input, hidden, return_h=False):
+        emb = embedded_dropout(self.encoder, input, dropout=self.dropoute if self.training else 0)
+        #emb = self.idrop(emb)
+
+        emb = self.lockdrop(emb, self.dropouti)
+
+        raw_output = emb
+        new_hidden = []
+        #raw_output, hidden = self.rnn(emb, hidden)
+        raw_outputs = []
+        outputs = []
+        for l, rnn in enumerate(self.rnns):
+            current_input = raw_output
+            raw_output, new_h = rnn(raw_output, hidden[l])
+            new_hidden.append(new_h)
+            raw_outputs.append(raw_output)
+            if l != self.nlayers - 1:
+                #self.hdrop(raw_output)
+                raw_output = self.lockdrop(raw_output, self.dropouth)
+                outputs.append(raw_output)
+        hidden = new_hidden
+
+        output = self.lockdrop(raw_output, self.dropout)
+        outputs.append(output)
+
+        result = output.view(output.size(0)*output.size(1), output.size(2))
+        if return_h:
+            return result, hidden, raw_outputs, outputs
+        return result, hidden
+
+    def init_hidden(self, bsz):
+        weight = next(self.parameters()).data
+        if self.rnn_type == 'LSTM':
+            return [(weight.new(1, bsz, self.nhid if l != self.nlayers - 1 else (self.ninp if self.tie_weights else self.nhid)).zero_(),
+                    weight.new(1, bsz, self.nhid if l != self.nlayers - 1 else (self.ninp if self.tie_weights else self.nhid)).zero_())
+                    for l in range(self.nlayers)]
+        elif self.rnn_type == 'QRNN' or self.rnn_type == 'GRU':
+            return [weight.new(1, bsz, self.nhid if l != self.nlayers - 1 else (self.ninp if self.tie_weights else self.nhid)).zero_()
+                    for l in range(self.nlayers)]
